c05a0a13411f03c688ba3998b7f52a319a7584e2
sanzid-hasan-230994 <sanzid-hasan-230994>
2024-01-09 08:16:33 +0000

    Initial commit

---
+++ src/test/java/com/nagad/sanzid/tut/Micronaut_REST_API_TUTTest.java
@@ -0,0 +1,21 @@
+package com.nagad.sanzid.tut;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class Micronaut_REST_API_TUTTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
